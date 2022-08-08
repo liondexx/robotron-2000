522663dae10e2029cf6f2ede56be5e554b7f3e90
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-08 16:09:11 -0300

    primeiro commit, adiciondo priemiras funções com java script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # robotron-2000
+> neste projeto aprenderemos a manipular o dom no app robotron-2000
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+const subtrair = document.querySelector("#subtrair")
+
+somar.addEventListener.parseInt(braco.value) + 1;
