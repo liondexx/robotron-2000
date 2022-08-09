4a40bc9719212234bfe267ec03b692b327064903
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-09 18:45:39 -0300

    colocando as operações atrravés de uma função, otimzanddo o código

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@ const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 const subtrair = document.querySelector("#subtrair")
 
-somar.addEventListener.parseInt(braco.value) + 1;
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1;
+})
+
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1;
+})
+
+function manipulaDados(operacao) {
+    if(operacao === subtrair) {
+        braco.value = parseInt(braco.value) + 1;
+    } else {
+        braco.value = parseInt(braco.value) - 1;
+    }
+}
