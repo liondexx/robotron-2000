5030db4b358a743db5fbe9406bc507ce155e08fd
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-09 18:48:54 -0300

    removendo código desnecessario

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const braco = document.querySelector("#braco")
 const subtrair = document.querySelector("#subtrair")
 
 somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
+   
 })
 
 subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
+    
 })
 
 function manipulaDados(operacao) {
