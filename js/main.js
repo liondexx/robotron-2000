7dd7a312653b42d7270a112b7707f02168fd1856
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-10 15:37:24 -0300

    tornado o código melhor usando o data-atributes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")//quando for buscado um elmento pelo "data-atribute" deve ser entr [] e sempre entre ""
 
 
 controle.forEach((elemnto)=> {
     elemnto.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)//dataset.controle ta puxando o elemento pelo data atribute
     })
 })
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector(".controle-contador")
+    const peca = controle.querySelector("[data-contador]")
 
     if(operacao === "-") {
         peca.value = parseInt(peca.value) - 1;
