e9c7590fc9612005dd85b3d1e2946d69009981d0
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-11 18:45:56 -0300

    terminando o resto do projeto, agora só falta as novas fetures

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 const controle = document.querySelectorAll("[data-controle]")//quando for buscado um elmento pelo "data-atribute" deve ser entr [] e sempre entre ""
+const estatisticas = document.querySelectorAll("[data-estatistica]")
 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-controle.forEach((elemnto)=> {
-    elemnto.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)//dataset.controle ta puxando o elemento pelo data atribute
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((elemento)=> {
+    elemento.addEventListener("click", (evento) =>{
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)//dataset.controle ta puxando o elemento pelo data atribute´
+        console.log(evento.target.dataset.controle)
+        atualizaEstatisticas(evento.target.dataset.controle, evento.target.dataset.peca)
+        
     })
 })
 
@@ -15,4 +52,22 @@ function manipulaDados(operacao, controle) {
     } else {
         peca.value = parseInt(peca.value) + 1;
     }
+}
+
+/*function atualizaEstatisticas(peca) {
+    
+    estatisticas.forEach((elemento) => {
+        console.log(elemento.dataset.estatistica)
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+    console.log(pecas[peca])
+}*/
+function atualizaEstatisticas(operacao, peca) {
+    if (operacao === "+") {
+        estatisticas.forEach( (elemento ) => {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        })
+    }else estatisticas.forEach( (elemento ) => {
+        elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+    })
 }
